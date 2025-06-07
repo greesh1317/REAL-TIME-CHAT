dd6e32b0ff06ba710642c89b9aaecce8231df869
greesh1317 <solasagreeshmadivyasree@gmail.com>
2025-06-07 17:28:37 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// Health check route
+
 router.get("/", (req, res) => {
   res.status(200).json({ response: "Server is up and running." });
 });
